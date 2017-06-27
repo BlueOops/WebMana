cdf08e701ecf5ef95dfdbb284bcda9a4721400a3
Justy <justy@ys-infoware.com>
2017-06-27 09:38:31 +0800

    調整模組Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './_comm/auth.guard';
-import { HomeComponent, LoginComponent ,MasterComponent } from './home/index';
+import { HomeComponent, LoginComponent, MasterComponent } from './home/index';
 
 const routes: Routes = [
   {
     path: '', component: MasterComponent,
     children: [                                                       //有套master的放下面，沒套的放外面
-      { path: '', component: HomeComponent  },
+      { path: '', component: HomeComponent },
       { path: 'home', component: HomeComponent },
       // { path: 'my', component: MyComponent },
       // { path: '', redirectTo: 'forms/reactvie', pathMatch: 'full' },
@@ -23,6 +23,11 @@ const routes: Routes = [
       {
         path: 'webmana',
         loadChildren: './webmana/webmana.module#WebmanaModule',
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'ecp',
+        loadChildren: './ecp/ecp.module#EcpModule',
         canActivate: [AuthGuard]
       }
     ]
--- src/app/webmana/webmana.module.ts
+++ src/app/webmana/webmana.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WebmanaRoutingModule } from './webmana-routing.module';
-import { WmMasterComponent } from './wm-master/wm-master.component';
 import { MenuComponent } from './menu/menu.component';
 import { WmAsideComponent } from './wm-aside/wm-aside.component';
 
@@ -11,6 +10,6 @@ import { WmAsideComponent } from './wm-aside/wm-aside.component';
     CommonModule,
     WebmanaRoutingModule
   ],
-  declarations: [WmMasterComponent, MenuComponent, WmAsideComponent]
+  declarations: [MenuComponent, WmAsideComponent]
 })
 export class WebmanaModule { }
